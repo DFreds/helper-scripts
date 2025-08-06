9e41a42ccec556b8defa52886aaa838b0f7b7686
Derek Fredrickson <dfreds08@gmail.com>
2025-08-06 15:13:23 -0400

    Update pf2e repo branch to v13-dev

--- build/update-pf2e-types.ts
+++ build/update-pf2e-types.ts
@@ -3,4 +3,4 @@ import { exec } from "child_process";
 // @ts-expect-error - This is a JSON file, not a TypeScript file
 import { pf2eRepoPath } from "../foundryconfig.json" with { type: "json" };
 
-exec(`cd ${pf2eRepoPath} && git checkout master && git pull`);
+exec(`cd ${pf2eRepoPath} && git checkout v13-dev && git pull`);
