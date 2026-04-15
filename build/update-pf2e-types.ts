fa0af6ca2c41375be500aa6d1e27e0d66c78ad3f
Derek Fredrickson <dfreds08@gmail.com>
2026-04-15 11:11:34 -0400

    Add pf2e branch to foundry config and use it when updating

--- build/update-pf2e-types.ts
+++ build/update-pf2e-types.ts
@@ -1,6 +1,6 @@
 import { exec } from "child_process";
 
 // @ts-expect-error - This is a JSON file, not a TypeScript file
-import { pf2eRepoPath } from "../foundryconfig.json" with { type: "json" };
+import { pf2eRepoPath, pf2eBranch } from "../foundryconfig.json" with { type: "json" };
 
-exec(`cd ${pf2eRepoPath} && git checkout v13-dev && git pull`);
+exec(`cd ${pf2eRepoPath} && git checkout ${pf2eBranch} && git pull`);
